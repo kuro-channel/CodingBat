3b4440126f2ec30b062e6b55b055c741c88b4941
kuro-channel <takumikuroiwa@gmail.com>
2017-11-06 23:53:18 +0900

    11/6 作業分

---
+++ src/Logic1.java
@@ -0,0 +1,46 @@
+
+public class Logic1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	// When squirrels get together for a party, they like to have cigars.
+	// A squirrel party is successful when the number of cigars is between 40 and 60,
+	// inclusive. Unless it is the weekend, in which case there is no upper bound on
+	// the number of cigars. Return true if the party with the given values is successful,
+	// or false otherwise.
+	//
+	// cigarParty(30, false) → false
+	// cigarParty(50, false) → true
+	// cigarParty(70, true) → true
+	public boolean cigarParty(int cigars, boolean isWeekend) {
+		if(isWeekend){
+			if(cigars >= 40){
+				return true;
+			}
+		}else {
+			if(cigars >= 40 && cigars <=60){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//	You and your date are trying to get a table at a restaurant.
+	//	The parameter "you" is the stylishness of your clothes,
+	//	in the range 0..10, and "date" is the stylishness of your date's clothes.
+	//	The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes.
+	//	If either of you is very stylish, 8 or more, then the result is 2 (yes).
+	//	With the exception that if either of you has style of 2 or less,
+	//	then the result is 0 (no). Otherwise the result is 1 (maybe).
+
+	//	dateFashion(5, 10) → 2
+	//	dateFashion(5, 2) → 0
+	//	dateFashion(5, 5) → 1
+//	public int dateFashion(int you, int date) {
+//
+//	}
+
+}

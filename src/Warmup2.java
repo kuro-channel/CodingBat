df2691de583a4464fe5910f104e717ee2377b129
kuro-channel <takumikuroiwa@gmail.com>
2017-11-19 19:06:08 +0900

    11/19 作業分

--- src/Warmup2.java
+++ src/Warmup2.java
@@ -328,8 +328,19 @@ public class Warmup2 {
 	//	has271([1, 2, 7, 1]) → true
 	//	has271([1, 2, 8, 1]) → false
 	//	has271([2, 7, 1]) → true
-//	public boolean has271(int[] nums) {
-//
-//	}
+	public boolean has271(int[] nums) {
+		// Iterate < length-2, so can use i+1 and i+2 in the loop.
+		// Return true immediately when seeing 271.
+		for (int i=0; i < (nums.length-2); i++) {
+			int val = nums[i];
+			if (nums[i+1] == (val+5) &&              // the "7" check
+					Math.abs(nums[i+2] - (val-1)) <= 2) {  // the "1" check
+				return true;
+			}
+		}
+
+		// If we get here ... none found.
+		return false;
+	}
 
 }
